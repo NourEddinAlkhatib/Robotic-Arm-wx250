6a12ed67e5bcc9c131a9ebb5581e6905dc1f9e09
AronGeorge05 <128283250+AronGeorge05@users.noreply.github.com>
2023-03-19 12:16:44 +0400

    Update and rename test3.py to final_code.py

--- final_code.py
+++ final_code.py
@@ -5,12 +5,10 @@ from interbotix_perception_modules.armtag import InterbotixArmTagInterface
 from interbotix_perception_modules.pointcloud import InterbotixPointCloudInterface
 
 # This script uses a color/depth camera to get the arm to find objects and pick them up.
-# For this demo, the arm is placed to the left of the camera facing outward. When the
-# end-effector is located at x=0, y=-0.3, z=0.2 w.r.t. the 'wx200/base_link' frame, the AR
-# tag should be clearly visible to the camera. A small basket should also be placed in front of the arm.
+# This program scans the space after the arm tries to pick three objects one time.
 #
-# To get started, open a terminal and type 'roslaunch interbotix_xsarm_perception xsarm_perception.launch robot_model:=wx200'
-# Then change to this directory and type 'python pick_place.py'
+# To get started, open a terminal and type 'roslaunch interbotix_xsarm_perception xsarm_perception.launch robot_model:=wx250'
+# Then change to this directory and type 'python3 final_code.py'
 
 def main():
     # Initialize the arm module along with the pointcloud and armtag modules
@@ -18,7 +16,7 @@ def main():
     pcl = InterbotixPointCloudInterface()
     armtag = InterbotixArmTagInterface()
 
-    # set initial arm and gripper pose
+    # set gripper pose
     bot.gripper.open()
 
     # get the ArmTag pose
@@ -26,17 +24,14 @@ def main():
     time.sleep(0.5)
     armtag.find_ref_to_arm_base_transform()
     bot.arm.go_to_sleep_pose()
-#    bot.arm.set_ee_pose_components(x=0.3, z=0.2)
 
     # get the cluster positions
-    # sort them from max to min 'x' position w.r.t. the 'wx200/base_link' frame
-#    while True
+    # sort them from min to max 'x' position w.r.t. the 'wx250/base_link' frame
     success, clusters = pcl.get_cluster_positions(ref_frame="wx250/base_link", sort_axis="x", reverse=False)
     while success == True:
         success, clusters = pcl.get_cluster_positions(ref_frame="wx250/base_link", sort_axis="x", reverse=False)
         if success == True:
-    # pick up all the objects and drop them in a virtual basket in front of the robot
-#    if success == True:
+            # pick up all the objects and drop them in a virtual basket next to the robot
             counter = 0
             for cluster in clusters:
                 counter = counter + 1
@@ -58,7 +53,6 @@ def main():
                         break
                 elif 0.12>z and z>=0.05:
                     bot.arm.set_ee_pose_components(x=x-0.04, y=y+0.01, z=z+0.05, pitch=0)
-                #    bot.arm.set_ee_pose_components(x=x-0.04, y=y+0.01, z=z-0.005, pitch=0)
                     bot.arm.set_ee_pose_components(x=x-0.02, y=y+0.01, z=z-0.035, pitch=0)
                     bot.gripper.close()
                     time.sleep(1)
@@ -75,12 +69,8 @@ def main():
                         break
                 elif z<0.05:
                     bot.arm.set_ee_pose_components(x=x, y=y+0.01, z=z+0.1, pitch=1.5)
-                #    bot.arm.set_ee_pose_components(x=x, y=y+0.01, z=z+0.08, pitch=1.5)
                     bot.arm.set_ee_pose_components(x=x, y=y+0.01, z=z+0.06, pitch=1.5, roll=0)
                     bot.arm.set_ee_pose_components(x=x, y=y+0.01, z=z+0.05, pitch=1.5, roll=1.5)
-                #    bot.arm.set_ee_pose_components(x=x, y=y+0.01, z=z+0.06, pitch=1.5, roll=0)
-                #    bot.arm.set_ee_pose_components(x=x, y=y+0.01, z=z+0.06, pitch=1.5, roll=-1.5)
-                #    bot.arm.set_ee_pose_components(x=x, y=y+0.01, z=z+0.05, pitch=1.5)
                     bot.gripper.close()
                     time.sleep(1)
                     bot.arm.set_ee_pose_components(x=0.25, y=0, z=0.35, pitch=0, roll=1.5)
@@ -98,10 +88,6 @@ def main():
             bot.arm.go_to_sleep_pose()
         else:
             break
-
-#    else:
-#        break
-#        sys.exit()
 
 if __name__=='__main__':
     main()
