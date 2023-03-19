ac6e9df6e0022d77bb4634c4cbed809603dd2473
AronGeorge05 <128283250+AronGeorge05@users.noreply.github.com>
2023-03-19 12:09:26 +0400

    Update test.py

--- test.py
+++ test.py
@@ -5,12 +5,10 @@ from interbotix_perception_modules.armtag import InterbotixArmTagInterface
 from interbotix_perception_modules.pointcloud import InterbotixPointCloudInterface
 
 # This script uses a color/depth camera to get the arm to find objects and pick them up.
-# For this demo, the arm is placed to the left of the camera facing outward. When the
-# end-effector is located at x=0, y=-0.3, z=0.2 w.r.t. the 'wx200/base_link' frame, the AR
-# tag should be clearly visible to the camera. A small basket should also be placed in front of the arm.
+# This program scans the space after the arm tries to pick all the objects one time, then tries to pick them up again.
 #
-# To get started, open a terminal and type 'roslaunch interbotix_xsarm_perception xsarm_perception.launch robot_model:=wx200'
-# Then change to this directory and type 'python pick_place.py'
+# To get started, open a terminal and type 'roslaunch interbotix_xsarm_perception xsarm_perception.launch robot_model:=wx250'
+# Then change to this directory and type 'python3 version1.py'
 
 def main():
     # Initialize the arm module along with the pointcloud and armtag modules
@@ -18,7 +16,7 @@ def main():
     pcl = InterbotixPointCloudInterface()
     armtag = InterbotixArmTagInterface()
 
-    # set initial arm and gripper pose
+    # set gripper pose
     bot.gripper.open()
 
     # get the ArmTag pose
@@ -26,59 +24,58 @@ def main():
     time.sleep(0.5)
     armtag.find_ref_to_arm_base_transform()
     bot.arm.go_to_sleep_pose()
-#    bot.arm.set_ee_pose_components(x=0.3, z=0.2)
 
     # get the cluster positions
-    # sort them from max to min 'x' position w.r.t. the 'wx200/base_link' frame
-#    while True
+    # sort them from min to max 'x' position w.r.t. the 'wx250/base_link' frame
+    while True
     success, clusters = pcl.get_cluster_positions(ref_frame="wx250/base_link", sort_axis="x", reverse=True)
-    # pick up all the objects and drop them in a virtual basket in front of the robot
-#    if success == True:
-    for cluster in reversed(clusters):
-            x, y, z = cluster["position"]
-            if z>=0.12:
-                bot.arm.set_ee_pose_components(x=x-0.03, y=y+0.02, z=z+0.05, pitch=0)
-                bot.arm.set_ee_pose_components(x=x-0.03, y=y+0.02, z=z-0.075, pitch=0)
-                bot.gripper.close()
-                time.sleep(0.5)
-                bot.arm.set_ee_pose_components(x=x-0.03, y=y+0.015, z=z-0.075, pitch=0, roll=1.5)
-                bot.arm.set_ee_pose_components(x=0.45, y=0, z=0.35, pitch=0, roll=1.5)
-                bot.arm.set_ee_pose_components(x=0.3, y=0.25, z=0.35, pitch=0, roll=1.5)
-                bot.arm.set_ee_pose_components(x=0.3, y=0.25, z=0.35, pitch=0, roll=0)
-                bot.gripper.open()
-                bot.arm.go_to_home_pose()
-                bot.arm.go_to_sleep_pose()
-                pcl.get_cluster_positions(ref_frame="wx250/base_link", sort_axis="x", reverse=True)
-            elif 0.12>z and z>=0.04:
-                bot.arm.set_ee_pose_components(x=x-0.03, y=y+0.02, z=z+0.05, pitch=0)
-                bot.arm.set_ee_pose_components(x=x-0.03, y=y+0.02, z=z-0.05, pitch=0)
-                bot.gripper.close()
-                time.sleep(0.5)
-                bot.arm.set_ee_pose_components(x=x-0.03, y=y+0.015, z=z+0.1, pitch=0)
-                bot.arm.go_to_home_pose()
-                bot.arm.set_ee_pose_components(x=0.3, y=0.25, z=0.35, pitch=0)
-                bot.gripper.open()
-                bot.arm.go_to_home_pose()
-                bot.arm.go_to_sleep_pose()
-                pcl.get_cluster_positions(ref_frame="wx250/base_link", sort_axis="x", reverse=True)
-            elif z<0.04:
-                bot.arm.set_ee_pose_components(x=x, y=y+0.01, z=z+0.1, pitch=1.5)
-                bot.arm.set_ee_pose_components(x=x, y=y+0.01, z=z+0.03, pitch=1.5)
-                bot.gripper.close()
-                time.sleep(0.5)
-                bot.arm.set_ee_pose_components(x=0.25, y=0, z=0.31, pitch=1.5)
-                bot.arm.set_ee_pose_components(x=0.25, y=0.25, z=0.31, pitch=1.5)
-                bot.gripper.open()
-                bot.arm.set_ee_pose_components(x=0.25, y=0.25, z=0.31, pitch=0)
-                bot.arm.go_to_home_pose()
-                bot.arm.go_to_sleep_pose()
-                pcl.get_cluster_positions(ref_frame="wx250/base_link", sort_axis="x", reverse=True)
-            else:
-                bot.arm.go_to_sleep_pose()
-    bot.arm.go_to_sleep_pose()
-#    else:
-#        break
-#        sys.exit()
+    # pick up all the objects and drop them in a virtual basket in next to the robot
+    if success == True:
+        for cluster in reversed(clusters):
+                x, y, z = cluster["position"]
+                if z>=0.12:
+                    bot.arm.set_ee_pose_components(x=x-0.03, y=y+0.02, z=z+0.05, pitch=0)
+                    bot.arm.set_ee_pose_components(x=x-0.03, y=y+0.02, z=z-0.075, pitch=0)
+                    bot.gripper.close()
+                    time.sleep(0.5)
+                    bot.arm.set_ee_pose_components(x=x-0.03, y=y+0.015, z=z-0.075, pitch=0, roll=1.5)
+                    bot.arm.set_ee_pose_components(x=0.45, y=0, z=0.35, pitch=0, roll=1.5)
+                    bot.arm.set_ee_pose_components(x=0.3, y=0.25, z=0.35, pitch=0, roll=1.5)
+                    bot.arm.set_ee_pose_components(x=0.3, y=0.25, z=0.35, pitch=0, roll=0)
+                    bot.gripper.open()
+                    bot.arm.go_to_home_pose()
+                    bot.arm.go_to_sleep_pose()
+                    pcl.get_cluster_positions(ref_frame="wx250/base_link", sort_axis="x", reverse=True)
+                elif 0.12>z and z>=0.04:
+                    bot.arm.set_ee_pose_components(x=x-0.03, y=y+0.02, z=z+0.05, pitch=0)
+                    bot.arm.set_ee_pose_components(x=x-0.03, y=y+0.02, z=z-0.05, pitch=0)
+                    bot.gripper.close()
+                    time.sleep(0.5)
+                    bot.arm.set_ee_pose_components(x=x-0.03, y=y+0.015, z=z+0.1, pitch=0)
+                    bot.arm.go_to_home_pose()
+                    bot.arm.set_ee_pose_components(x=0.3, y=0.25, z=0.35, pitch=0)
+                    bot.gripper.open()
+                    bot.arm.go_to_home_pose()
+                    bot.arm.go_to_sleep_pose()
+                    pcl.get_cluster_positions(ref_frame="wx250/base_link", sort_axis="x", reverse=True)
+                elif z<0.04:
+                    bot.arm.set_ee_pose_components(x=x, y=y+0.01, z=z+0.1, pitch=1.5)
+                    bot.arm.set_ee_pose_components(x=x, y=y+0.01, z=z+0.03, pitch=1.5)
+                    bot.gripper.close()
+                    time.sleep(0.5)
+                    bot.arm.set_ee_pose_components(x=0.25, y=0, z=0.31, pitch=1.5)
+                    bot.arm.set_ee_pose_components(x=0.25, y=0.25, z=0.31, pitch=1.5)
+                    bot.gripper.open()
+                    bot.arm.set_ee_pose_components(x=0.25, y=0.25, z=0.31, pitch=0)
+                    bot.arm.go_to_home_pose()
+                    bot.arm.go_to_sleep_pose()
+                    pcl.get_cluster_positions(ref_frame="wx250/base_link", sort_axis="x", reverse=True)
+                else:
+                    bot.arm.go_to_sleep_pose()
+        bot.arm.go_to_sleep_pose()
+    else:
+        break
+        sys.exit()
 
 if __name__=='__main__':
     main()
